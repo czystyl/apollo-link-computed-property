9d111e48139fe36ba9e9f02712c10442018e8f5f
Czysty <lukasz.czyszczonik@gmail.com>
2018-04-20 22:32:55 +0200

    feat: Nested computed property

--- __tests__/computed.test.js
+++ __tests__/computed.test.js
@@ -33,11 +33,61 @@ test('return computed property', async () => {
         me {
           firstName
           lastName
-          fullName @computed(value: "$firstName _&_&_ $lastName")
+          fullName @computed(value: "$me.firstName _&_&_ $me.lastName")
         }
       }
     `,
   });
 
   expect(response).toMatchSnapshot();
+});
+
+test('return computed property for nested type', async () => {
+  nock('http://localhost:3003', { encodedQueryParams: true })
+    .post('/graphql', {
+      operationName: null,
+      variables: {},
+      query:
+        '{\n  me {\n    firstName\n    lastName\n    department {\n      name\n      code\n      __typename\n    }\n    __typename\n  }\n}\n',
+    })
+    .reply(200, {
+      data: {
+        me: {
+          firstName: 'John',
+          lastName: 'Doe',
+          department: {
+            name: 'Security',
+            code: 1111,
+            __typename: 'Department',
+          },
+          __typename: 'User',
+        },
+      },
+    });
+
+  const response = await client.query({
+    query: gql`
+      {
+        me {
+          firstName
+          lastName
+          fullName @computed(value: "$me.firstName $me.lastName")
+          department {
+            access
+              @computed(
+                value: "Access to $me.department.name: $me.department.code"
+              )
+            name
+            code
+          }
+        }
+      }
+    `,
+  });
+
+  expect(response.data.me.fullName).toEqual('John Doe');
+  expect(response.data.me.department.access).toEqual(
+    'Access to Security: 1111'
+  );
+  // expect(response).toMatchSnapshot();
 });
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { ApolloLink, Observable } = require('apollo-link');
 const set = require('lodash.set');
+const get = require('lodash.get');
+const flatten = require('lodash.flatten');
 const {
   removeDirectivesFromDocument,
   getDirectivesFromDocument,
@@ -15,9 +17,18 @@ class ComputedPropertyLink extends ApolloLink {
     this.mainDefinitionName = '';
   }
 
-  _getDirectiveDefinitionFromField(field) {
+  _getDirectiveDefinitionFromField(mainDefinitionName, field) {
+    if (field.kind === 'Field' && field.selectionSet) {
+      return field.selectionSet.selections.map(subField =>
+        this._getDirectiveDefinitionFromField(
+          `${mainDefinitionName}.${field.name.value}`,
+          subField
+        )
+      );
+    }
+
     return {
-      name: field.name.value,
+      name: `${mainDefinitionName}.${field.name.value}`,
       value: field.directives[0].arguments[0].value.value,
     };
   }
@@ -29,7 +40,11 @@ class ComputedPropertyLink extends ApolloLink {
       this.mainDefinitionName = definition.name.value;
       const fieldsFromDocument = definition.selectionSet.selections;
 
-      return fieldsFromDocument.map(this._getDirectiveDefinitionFromField);
+      const mainDefinitionName = definition.name.value;
+
+      return fieldsFromDocument.map(field =>
+        this._getDirectiveDefinitionFromField(mainDefinitionName, field)
+      );
     }
 
     return null;
@@ -60,6 +75,7 @@ class ComputedPropertyLink extends ApolloLink {
     checkDocument(doc);
 
     const onlyDirectiveDeclaration = this._getDirectivesFromDoc(doc);
+
     const mainDefinition = getMainDefinition(onlyDirectiveDeclaration);
 
     return this._getComputedFields(mainDefinition);
@@ -79,32 +95,22 @@ class ComputedPropertyLink extends ApolloLink {
 
       return obs.subscribe({
         next: response => {
-          const data = response.data[this.mainDefinitionName];
+          const { data } = response;
 
-          for (const resField in data) {
-            if (Object.prototype.hasOwnProperty.call(data, resField)) {
-              directiveSetting.forEach(computed => {
-                if (!data[computed.name]) {
-                  set(
-                    response,
-                    `data.${this.mainDefinitionName}.${computed.name}`,
-                    computed.value
-                  );
-                }
-
-                const value = data[computed.name].replace(
-                  `$${resField}`,
-                  data[resField]
-                );
-
-                set(
-                  response,
-                  `data.${this.mainDefinitionName}.${computed.name}`,
-                  value
-                );
-              });
+          flatten(directiveSetting).forEach(computed => {
+            if (!data[computed.name]) {
+              set(response, `data.${computed.name}`, computed.value);
             }
-          }
+
+            const regexp = /\$(\w+\.)+\w+/g;
+
+            const value = computed.value.replace(
+              regexp,
+              match => get(data, match.substring(1)) // remove $
+            );
+
+            set(response, `data.${computed.name}`, value);
+          });
 
           observer.next(response);
         },
