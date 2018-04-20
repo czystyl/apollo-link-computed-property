ad8fa95ffbe23d0decebbb3608f50e6a6f348115
Luke Czyszczonik <lukasz.czyszczonik@gmail.com>
2018-04-20 10:26:13 +0200

    Initial implementation (#1)
    * Initial implementation
    
    * Change CircleCi workspace
    
    * Extract getting directive settings
    
    * Extract getting directive settings
    
    * Update peer dependencies

--- index.js
+++ index.js
@@ -1,41 +1,114 @@
 const { ApolloLink, Observable } = require('apollo-link');
+const set = require('lodash.set');
 const {
   removeDirectivesFromDocument,
   getDirectivesFromDocument,
   hasDirectives,
+  getMainDefinition,
+  checkDocument,
 } = require('apollo-utilities');
 
 class ComputedPropertyLink extends ApolloLink {
-  request(operation, forward) {
-    const isComputedQuery = hasDirectives(['computed'], operation.query);
+  constructor(name = 'computed') {
+    super();
+    this.directiveName = name;
+    this.mainDefinitionName = '';
+  }
 
-    if (!isComputedQuery) {
+  _getDirectiveDefinitionFromField(field) {
+    return {
+      name: field.name.value,
+      value: field.directives[0].arguments[0].value.value,
+    };
+  }
+
+  _getComputedFields(mainDefinition) {
+    if (mainDefinition.selectionSet) {
+      const definition = mainDefinition.selectionSet.selections[0];
+
+      this.mainDefinitionName = definition.name.value;
+      const fieldsFromDocument = definition.selectionSet.selections;
+
+      const fields = fieldsFromDocument.map(
+        this._getDirectiveDefinitionFromField
+      );
+      return fields;
+    }
+
+    return null;
+  }
+
+  _hasComputedDirective(doc) {
+    checkDocument(doc);
+
+    return hasDirectives([this.directiveName], doc);
+  }
+
+  _removeDirective(doc) {
+    checkDocument(doc);
+
+    return removeDirectivesFromDocument(
+      [{ name: this.directiveName, remove: true }],
+      doc
+    );
+  }
+
+  _getDirectivesFromDoc(doc) {
+    checkDocument(doc);
+
+    return getDirectivesFromDocument([{ name: this.directiveName }], doc);
+  }
+
+  _getDirectiveSettings(doc) {
+    checkDocument(doc);
+
+    const onlyDirectiveDeclaration = this._getDirectivesFromDoc(doc);
+    const mainDefinition = getMainDefinition(onlyDirectiveDeclaration);
+
+    return this._getComputedFields(mainDefinition);
+  }
+
+  request(operation, forward) {
+    if (!this._hasComputedDirective(operation.query)) {
       return forward(operation);
     }
 
-    const newQuery = removeDirectivesFromDocument(
-      [{ name: 'computed', remove: true }],
-      operation.query
-    );
-    // @TODO: handle directive from query
-    // const dir = getDirectivesFromDocument(
-    //   [{ name: 'computed' }],
-    //   operation.query
-    // );
-    //
-    // const directives =
-    //   dir.definitions[0].selectionSet.selections[0].selectionSet.selections[0]
-    //     .directives;
-    //
-    // console.log(directives); //
+    const directiveSetting = this._getDirectiveSettings(operation.query);
 
-    operation.query = newQuery;
+    operation.query = this._removeDirective(operation.query);
 
     return new Observable(observer => {
       const obs = forward(operation);
 
       return obs.subscribe({
         next: response => {
+          const data = response.data[this.mainDefinitionName];
+
+          for (const resField in data) {
+            if (Object.prototype.hasOwnProperty.call(data, resField)) {
+              directiveSetting.forEach(computed => {
+                if (!data[computed.name]) {
+                  set(
+                    response,
+                    `data.${this.mainDefinitionName}.${computed.name}`,
+                    computed.value
+                  );
+                }
+
+                const value = data[computed.name].replace(
+                  `$${resField}`,
+                  data[resField]
+                );
+
+                set(
+                  response,
+                  `data.${this.mainDefinitionName}.${computed.name}`,
+                  value
+                );
+              });
+            }
+          }
+
           observer.next(response);
         },
       });
